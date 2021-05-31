6c3d16e5600d31d071ed82ac8c71be88612a73b7
Muhammad Omran <use.yurMind@gmail.com>
2021-05-31 23:26:48 +0200

    structure the project add the nessecary components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React from "react";
 import { Route } from "react-router";
-// import * as BooksAPI from './BooksAPI'
 import "./App.css";
+import { getAll } from "./BooksAPI";
 import { Search } from "./components/search";
 import { NotFound } from "./pages/404";
 import { Home } from "./pages/home";
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      books: [],
+      shelves: ["Currently Reading", "Want to Read", "read", "None"],
+    };
+  }
+
+  componentDidMount() {
+    const fetchBooks = async () => {
+      const books = await getAll();
+      this.setState({ books });
+    };
+
+    fetchBooks();
+  }
 
   render() {
     return (
       <div className="app">
         <Route path="/search" component={Search} />
-        <Route exact path="/" component={Home} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <Home books={this.state.books} shelves={this.state.shelves} />
+          )}
+        />
         <Route path="/" component={NotFound} />
       </div>
     );
---
+++ src/components/book.js
@@ -0,0 +1,52 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Book = (props) => (
+  <div className="book">
+    <div className="book-top">
+      <div
+        className="book-cover"
+        style={{
+          width: 128,
+          height: 193,
+          backgroundImage: `url("${props.poster}")`,
+        }}
+      />
+      <div className="book-shelf-changer">
+        <select>
+          <option value="move" disabled>
+            Move to...
+          </option>
+          {props.options.map((o, i) => (
+            <option
+              key={i}
+              value={o}
+              checked={
+                props.shelf.toLowerCase() ===
+                o.replace(/\s/gi, "").toLowerCase()
+              }
+            >
+              {o}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+    <div className="book-title">{props.title}</div>
+    {props.authors.map((a, i) => (
+      <div className="book-authors" key={i}>
+        {a}
+      </div>
+    ))}
+  </div>
+);
+
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  authors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  poster: PropTypes.string.isRequired,
+  shelf: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Book;
